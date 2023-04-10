743befaaa4a9c315cf8d38575bdd0596253c1c03
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2023-04-10 01:43:21 +0100

    login & signup

--- Gestion_Pharmacie/src/application/Main.java
+++ Gestion_Pharmacie/src/application/Main.java
@@ -3,8 +3,10 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 
 
 
@@ -13,9 +15,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root= FXMLLoader.load(getClass().getResource("/interfaces/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			/*scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());*/
+			scene.setFill(Color.TRANSPARENT);
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ Gestion_Pharmacie/src/controllers/MainController.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+public class MainController {
+
+    @FXML
+    private javafx.scene.layout.VBox VBox;
+
+    @FXML
+    private Button btn_seconnecter;
+
+    @FXML
+    private Button btn_sinscrire;
+    
+    private Parent Page ;
+
+    @FXML
+    void On_SignIn() {
+    	TranslateTransition T = new TranslateTransition(Duration.seconds(1), VBox);
+    	T.setToX(VBox.getLayoutX()*7.2);
+    	T.play();
+    	T.setOnFinished(e -> {
+    		try {
+				Page = FXMLLoader.load(getClass().getResource("/interfaces/SignIn.fxml"));
+			    VBox.getChildren().removeAll();
+			    VBox.getChildren().setAll(Page);
+    		} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+    	});
+    }
+
+    @FXML
+    void On_SignUp() {
+    	TranslateTransition T = new TranslateTransition(Duration.seconds(1), VBox);
+    	T.setToX(0);
+    	T.play();
+    	T.setOnFinished(e -> {
+    		try {
+				Page = FXMLLoader.load(getClass().getResource("/interfaces/SignUp.fxml"));
+			    VBox.getChildren().removeAll();
+			    VBox.getChildren().setAll(Page);
+    		} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+    	});
+   
+    }
+    
+    public void initialize(URL location , ResourceBundle resourses) {
+    	TranslateTransition T = new TranslateTransition(Duration.seconds(1), VBox);
+    	T.setToX(VBox.getLayoutX()*5.5);
+    	T.play();
+    }
+
+}
