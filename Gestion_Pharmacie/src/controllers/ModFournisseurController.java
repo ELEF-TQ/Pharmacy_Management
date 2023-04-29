9b966da24280a091c814db9e68fd7330c336ab45
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2023-04-29 18:09:20 +0100

    add edit to supplier

--- Gestion_Pharmacie/src/controllers/ModFournisseurController.java
+++ Gestion_Pharmacie/src/controllers/ModFournisseurController.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
 import database.DBConnection;
@@ -14,16 +15,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import models.Fournisseur;
 
 public class ModFournisseurController implements Initializable{
@@ -192,6 +201,129 @@ public class ModFournisseurController implements Initializable{
 	        }
 	    }
 	    
+	  //____________ UPDATE Supplier informations  : 
+	    @FXML
+	    public void On_ModifySupplier() {
+	        Fournisseur selectedSupplier = Table_FRN.getSelectionModel().getSelectedItem();
+	        if (selectedSupplier != null) {
+	            Fournisseur editedSupplier = showEditSupplierDialog(selectedSupplier);
+	            if (editedSupplier != null) {
+	                // Update the supplier in the database
+	                String updateSQL = "UPDATE suppliers SET Name = ?, Phone = ?, Email = ?, Payment = ?, RIB = ? WHERE ID = ?";
+	                try {
+	                    PreparedStatement statement = (PreparedStatement) con.prepareStatement(updateSQL);
+	                    statement.setString(1, editedSupplier.getName());
+	                    statement.setString(2, editedSupplier.getPhone());
+	                    statement.setString(3, editedSupplier.getEmail());
+	                    statement.setString(4, editedSupplier.getPayment());
+	                    statement.setString(5, editedSupplier.getRIB());
+	                    statement.setInt(6, editedSupplier.getId());
+	                    statement.executeUpdate();
+
+	                    Alert alert = new Alert(AlertType.CONFIRMATION, "Fournisseur modifié avec succès");
+	                    alert.showAndWait();
+
+	                    // Refresh the supplier table view
+	                    showSuppliers();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                    Alert alert = new Alert(AlertType.WARNING, "Échec de la modification du fournisseur");
+	                    alert.showAndWait();
+	                }
+	            }
+	        } else {
+	            Alert alert = new Alert(AlertType.CONFIRMATION, "Pas de fournisseur sélectionné");
+	            alert.showAndWait();
+	        }
+	    }
+
+	    
+	    private Fournisseur showEditSupplierDialog(Fournisseur supplier) {
+	        // Create a new Stage for the dialog
+	        Stage dialogStage = new Stage();
+	        dialogStage.setTitle("Modifier les informations du fournisseur");
+
+	        // Create the GridPane for the dialog content
+	        GridPane gridPane = new GridPane();
+	        gridPane.setHgap(10);
+	        gridPane.setVgap(10);
+	        gridPane.setPadding(new Insets(20));
+
+	        // Create the input fields and labels for each supplier property
+	        TextField nameField = new TextField(supplier.getName());
+	        TextField phoneField = new TextField(supplier.getPhone());
+	        TextField emailField = new TextField(supplier.getEmail());
+	        ComboBox<String> paymentBox = new ComboBox<>();
+	        paymentBox.getItems().addAll("Payment Par Bank", "Payment Par Cash");
+	        paymentBox.setValue(supplier.getPayment());
+	        TextField ribField = new TextField(supplier.getRIB());
+
+	        // Add the input fields and labels to the GridPane
+	        gridPane.add(new Label("Nom:"), 0, 0);
+	        gridPane.add(nameField, 1, 0);
+	        gridPane.add(new Label("Téléphone:"), 0, 1);
+	        gridPane.add(phoneField, 1, 1);
+	        gridPane.add(new Label("Email:"), 0, 2);
+	        gridPane.add(emailField, 1, 2);
+	        gridPane.add(new Label("Paiement:"), 0, 3);
+	        gridPane.add(paymentBox, 1, 3);
+	        gridPane.add(new Label("RIB:"), 0, 4);
+	        gridPane.add(ribField, 1, 4);
+
+	        // Set the initial value of Rib_Frn based on the selected payment method
+	        if (supplier.getPayment().equals("Payment Par Cash")) {
+	            ribField.setText("Payment Par Cash");
+	        }
+
+	        // Enable/disable the RIB field based on the selected payment method
+	        paymentBox.setOnAction(e -> {
+	            String selectedPayment = paymentBox.getValue();
+	            ribField.setDisable(selectedPayment.equals("Payment Par Cash"));
+	            if (selectedPayment.equals("Payment Par Cash")) {
+	                ribField.setText("Payment Par Cash");
+	            } else {
+	                ribField.setText("");
+	            }
+	        });
+
+	        // Create the Save and Cancel buttons
+	        Button saveButton = new Button("Enregistrer");
+	        Button cancelButton = new Button("Annuler");
+
+	        // Set the Save button's action handler
+	        saveButton.setOnAction(e -> {
+	            supplier.setName(nameField.getText());
+	            supplier.setPhone(phoneField.getText());
+	            supplier.setEmail(emailField.getText());
+	            supplier.setPayment(paymentBox.getValue());
+	            supplier.setRIB(ribField.getText());
+	            dialogStage.close();
+	        });
+
+	        // Set the Cancel button's action handler
+	        cancelButton.setOnAction(e -> dialogStage.close());
+
+	        // Add the buttons to a HBox
+	        HBox buttonBox = new HBox(10);
+	        buttonBox.setAlignment(Pos.CENTER_RIGHT);
+	        buttonBox.getChildren().addAll(saveButton, cancelButton);
+
+	        // Add the GridPane and buttons to a VBox
+	        VBox vbox = new VBox(20);
+	        vbox.setPadding(new Insets(20));
+	        vbox.getChildren().addAll(gridPane, buttonBox);
+
+	        // Create the Scene and set it on the Stage
+	        Scene scene = new Scene(vbox);
+	        dialogStage.setScene(scene);
+
+	        // Show the dialog and wait for it to be closed
+	        dialogStage.showAndWait();
+
+	        return supplier;
+	    }
+
+
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
